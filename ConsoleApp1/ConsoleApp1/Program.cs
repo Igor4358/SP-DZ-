bf23f8aed33b553d44c4ac45ec06a14f6df426f3
Igor4358 <igor4358@yandex.ru>
2025-02-06 19:49:40 +0300

    C

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_DZ_5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                RunTasks();
+                Console.WriteLine(" все закончили");
+            }
+
+             void RunTasks()
+            {
+                // Start
+                Task task1 = new Task(DisplayCurrentDateTime);
+                task1.Start();
+                task1.Wait();
+
+                // StartNew
+                Task task2 = Task.Factory.StartNew(DisplayCurrentDateTime);
+                task2.Wait();
+
+                // Run
+                Task task3 = Task.Run(DisplayCurrentDateTime);
+                task3.Wait();
+            }
+
+             void DisplayCurrentDateTime()
+            {
+                Console.WriteLine($"Текущая {DateTime.Now}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/SP-DZ5-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_DZ5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Task task = Task.Run(() => DisplayPrimeNumbers(0, 1000));
+                task.Wait();
+                Console.WriteLine("отображено");
+            }
+
+             void DisplayPrimeNumbers(int start, int end)
+            {
+                Console.WriteLine("простые числа от 0 до 1000");
+
+                for (int number = start; number <= end; number++)
+                {
+                    if (IsPrime(number))
+                    {
+                        Console.WriteLine(number);
+                    }
+                }
+            }
+
+             bool IsPrime(int number)
+            {
+                if (number < 2) return false;
+
+                for (int i = 2; i * i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}

4c05e1bc3cce19f343922e1df187c82c6d35e8e4
Igor4358 <igor4358@yandex.ru>
2025-02-09 21:18:24 +0300

    Commit

---
+++ SP-DZ8-1/SP-DZ8-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP_DZ8_1
+{
+class Program
+        {
+            static void Main()
+            {
+                string filePath = "Probnic.txt";
+                var lines = File.ReadAllLines(filePath);
+                var numbers = lines.Select(int.Parse).ToList();
+                int uniqueCount = numbers.AsParallel().Distinct().Count();
+                Console.WriteLine($"Количество уникальных значений: {uniqueCount}");
+            }
+        }
+    }

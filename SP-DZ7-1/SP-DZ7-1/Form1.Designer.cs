d2391fbb54e6583816537c9e0a7a7f6256571607
Igor4358 <igor4358@yandex.ru>
2025-02-09 20:26:53 +0300

    Commit

---
+++ SP-DZ7-1/SP-DZ7-1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SP_DZ7_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.analyzeButton = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.radioButtonDisplayOnScreen = new System.Windows.Forms.RadioButton();
+            this.textBoxReport = new System.Windows.Forms.TextBox();
+            this.radioButtonSaveToFile = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(93, 202);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(75, 23);
+            this.analyzeButton.TabIndex = 0;
+            this.analyzeButton.Text = "button";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(93, 73);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(128, 22);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // radioButtonDisplayOnScreen
+            // 
+            this.radioButtonDisplayOnScreen.AutoSize = true;
+            this.radioButtonDisplayOnScreen.Location = new System.Drawing.Point(277, 203);
+            this.radioButtonDisplayOnScreen.Name = "radioButtonDisplayOnScreen";
+            this.radioButtonDisplayOnScreen.Size = new System.Drawing.Size(103, 20);
+            this.radioButtonDisplayOnScreen.TabIndex = 2;
+            this.radioButtonDisplayOnScreen.TabStop = true;
+            this.radioButtonDisplayOnScreen.Text = "radioButton1";
+            this.radioButtonDisplayOnScreen.UseVisualStyleBackColor = true;
+            // 
+            // textBoxReport
+            // 
+            this.textBoxReport.Location = new System.Drawing.Point(290, 72);
+            this.textBoxReport.Multiline = true;
+            this.textBoxReport.Name = "textBoxReport";
+            this.textBoxReport.Size = new System.Drawing.Size(332, 22);
+            this.textBoxReport.TabIndex = 3;
+            // 
+            // radioButtonSaveToFile
+            // 
+            this.radioButtonSaveToFile.AutoSize = true;
+            this.radioButtonSaveToFile.Location = new System.Drawing.Point(558, 202);
+            this.radioButtonSaveToFile.Name = "radioButtonSaveToFile";
+            this.radioButtonSaveToFile.Size = new System.Drawing.Size(103, 20);
+            this.radioButtonSaveToFile.TabIndex = 4;
+            this.radioButtonSaveToFile.TabStop = true;
+            this.radioButtonSaveToFile.Text = "radioButton1";
+            this.radioButtonSaveToFile.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButtonSaveToFile);
+            this.Controls.Add(this.textBoxReport);
+            this.Controls.Add(this.radioButtonDisplayOnScreen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.analyzeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button analyzeButton;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.RadioButton radioButtonDisplayOnScreen;
+        private System.Windows.Forms.TextBox textBoxReport;
+        private System.Windows.Forms.RadioButton radioButtonSaveToFile;
+    }
+}
+
---
+++ SP-DZ7-1/SP-DZ7-1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_DZ7_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void analyzeButton_Click(object sender, EventArgs e)
+        {
+            string text = textBoxInput.Text;
+            bool saveToFile = radioButtonSaveToFile.Checked;
+
+            var report = await Task.Run(() => AnalyzeText(text));
+
+            if (saveToFile)
+            {
+                SaveReportToFile(report);
+            }
+            else
+            {
+                DisplayReport(report);
+            }
+        }
+        private string AnalyzeText(string text)
+        {
+            int sentenceCount = text.Split(new[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            int charCount = text.Length;
+            int wordCount = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            int questionCount = text.Split('?').Length - 1;
+            int exclamationCount = text.Split('!').Length - 1;
+
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"Количество предложений: {sentenceCount}");
+            report.AppendLine($"Количество символов: {charCount}");
+            report.AppendLine($"Количество слов: {wordCount}");
+            report.AppendLine($"Количество вопросительных предложений: {questionCount}");
+            report.AppendLine($"Количество восклицательных предложений: {exclamationCount}");
+
+            return report.ToString();
+        }
+
+        private void DisplayReport(string report)
+        {
+            textBoxReport.Text = report;
+        }
+
+        private void SaveReportToFile(string report)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, report);
+                }
+            }
+        }
+    }
+}

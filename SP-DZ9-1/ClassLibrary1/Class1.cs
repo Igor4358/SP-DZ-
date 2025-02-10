5f0e2d99a2c2e7023050dd611685c7405399847a
Igor4358 <igor4358@yandex.ru>
2025-02-10 21:03:21 +0300

    Commit

---
+++ SP-DZ9-1/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace GeometryLibrary
+{
+    public class Geometry
+    {
+        public static double SquareArea(double side)
+        {
+            return side * side;
+        }
+        public static double RectangleArea(double width,double height)
+        {
+            return width * height;
+        }
+        public static double TriangleArea(double baseLength,double height)
+        {
+            return 0.5* baseLength * height;
+        }
+    }
+}
---
+++ SP-DZ9-1/SP-DZ9-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryLibrary;
+    
+namespace GeometryApp
+
+{ 
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                double squareSide = 5.0;
+                double rectangleWidth = 4.0;
+                double rectangleHeight = 6.0;
+                double triangleBase = 3.0;
+                double triangleHeight = 7.0;
+
+                Console.WriteLine($"Площадь квадрата {squareSide} это {Geometry.SquareArea(squareSide)}");
+                Console.WriteLine($"Площадь прямоугольника {rectangleWidth} и высота {rectangleHeight} это {Geometry.RectangleArea(rectangleWidth, rectangleHeight)}");
+                Console.WriteLine($"Площадь треугольника {triangleBase} и высота {triangleHeight} это {Geometry.TriangleArea(triangleBase, triangleHeight)}");
+            }
+        }
+    }
